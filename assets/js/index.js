7af88a7d3ddff78d2308205b833cf8b6e9d733e8
Igor Eliseo <higorfernandoeliseo2016@gmail.com>
2025-07-29 18:13:08 -0300

    finalizando o javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,19 @@ const db = new Dexie('PontoDB')
 db.version(1).stores({
     items: `++pontoid,data,hora,tipo`
 })
+
+const itemPonto = document.getElementById('itemPonto')
+const itemsDiv = document.getElementById('registroItem')
+
+itemPonto.onsubmit = async (e) => {
+    e.preventDefault()
+
+    const data_reg = document.getElementById('dia_registro')
+    const tipo_reg = document.getElementById('tipo_registro')
+    const hora_reg = document.getElementById('hora_registro')
+
+    await db.items.add({data_reg,tipo_reg,tipo_reg})
+
+    itemPonto.reset()
+
+}
