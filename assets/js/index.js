76694d31830d92908f8b060e38a8b072d0e28315
Igor Eliseo <higorfernandoeliseo2016@gmail.com>
2025-08-02 19:28:33 -0300

    update index.css, index.js e o index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,13 @@ const itemsDiv = document.getElementById('registrosBox')
 
 const db = new Dexie('PontoDB')
 db.version(1).stores({
-    items: `++pontoid,data,hora,tipo`
+    items: `++pontoid,data,hora,tipo`,
+    datas: `++dataid,&data`
 })
+
+// db.version(2).stores({
+//     datas: null
+// })
 
 const populateItemsDiv = async () => {
 
@@ -35,9 +40,77 @@ itemPonto.onsubmit = async (e) => {
     const tipo = document.querySelector('input[name="tipo_registro"]:checked').value;
     const hora = document.getElementById('hora_registro').value
 
-    await db.items.add({data,hora,tipo})
-    await populateItemsDiv()
+    if(datainput.length > 0 && tipo.length > 0 && hora.length > 0){
+        await db.items.add({data,hora,tipo})
+        await populateItemsDiv()
 
-    itemPonto.reset()
-
+        itemPonto.reset()
+    }
 }
+
+//const dados = db.items.where('data').equals('27/07/2025').toArray()
+//console.log(dados)
+
+// db.items.where('data').equals('27/07/2025').each(item => {
+//     console.log(`Hora: ${item.hora}`)
+// })
+
+// async function getAllItems() {
+
+//     const allPoints = await db.datas.reverse().toArray()
+//     const linhaPontos = []
+//     let strings = ''
+//     allPoints.forEach(point => {
+//         db.items.where('data').equals(point.data).each(item => {
+//             let texto = `Data: ${point.data} ${item.tipo}Hora: ${item.hora}`;
+//             linhaPontos.unshift(texto)
+//         })
+//     })
+
+//     //strings = linhaPontos.join('')
+//     console.log(linhaPontos)
+
+// }
+
+// getAllItems()
+
+
+
+async function gerarPDF() {
+
+    var doc = new jsPDF();
+
+    const registros = await db.items.toArray()
+
+    doc.setFontSize(14)
+    doc.text('Relatório de Pontos: mes/ano', 20, 20)
+
+    let y = 30
+
+    registros.forEach((points) =>{
+        doc.setFontSize(12)
+        doc.text(`Data: ${points.data}\n${points.tipo}\nHora: ${points.hora}\n\n `, 20, y)
+        y += 20
+        if(y > 280){
+            doc.addPage()
+            y = 20
+        }
+    });
+
+    // const datablob = await exportDexieData()
+    // if(!datablob) return;
+
+    // console.log(datablob)
+
+
+    // doc.text('Ola Mundo!!', 10, 10)
+    doc.save('relatorioPontos.pdf')
+}
+
+// allTimes.each(item => {
+//         console.log(`Hora: ${item.hora}`)
+//     })
+
+// db.items.where('data').equals(point.data).each(item => {
+//             console.log(`Hora: ${item.hora}`)
+//         })
